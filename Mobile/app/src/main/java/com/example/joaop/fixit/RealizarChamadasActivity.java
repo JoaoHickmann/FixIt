f543c80e70af46773ca6181fa31dc6603e3126af
guikipper <33471042+guikipper@users.noreply.github.com>
2017-11-11 15:05:18 -0200

    LAYOUTERSON

--- Mobile/app/src/main/java/com/example/joaop/fixit/RealizarChamadasActivity.java
+++ Mobile/app/src/main/java/com/example/joaop/fixit/RealizarChamadasActivity.java
@@ -170,7 +170,6 @@ public class RealizarChamadasActivity extends AppCompatActivity {
                         }
                     }
                 }).start();
-
             }
         });
     }
