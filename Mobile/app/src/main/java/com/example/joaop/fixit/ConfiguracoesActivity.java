ec8a403c4c72fb4952d21f6ccd6d8e2e6c062346
guikipper <33471042+guikipper@users.noreply.github.com>
2017-11-18 21:35:45 -0200

    Mais coisa das confg

--- Mobile/app/src/main/java/com/example/joaop/fixit/ConfiguracoesActivity.java
+++ Mobile/app/src/main/java/com/example/joaop/fixit/ConfiguracoesActivity.java
@@ -6,25 +6,77 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.io.IOException;
 
 public class ConfiguracoesActivity extends AppCompatActivity {
 
+    EditText etConfiguracoesNome, etConfiguracoesEmail, etConfiguracoesSenhaAtual, etConfiguracoesNovaSenha,
+            etConfiguracoesConfirmarSenha;
+    Button btConfiguracoesAlterarNome, btConfiguracoesAlterarSenha;
+    Dados dados;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_configuracoes);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        etConfiguracoesNome = findViewById(R.id.etConfiguracoesNome);
+        etConfiguracoesEmail = findViewById(R.id.etConfiguracoesEmail);
+        etConfiguracoesSenhaAtual = findViewById(R.id.etConfiguracoesSenhaAtual);
+        etConfiguracoesNovaSenha = findViewById(R.id.etConfiguracoesNovaSenha);
+        etConfiguracoesConfirmarSenha = findViewById(R.id.etConfiguracoesConfirmarNovaSenha);
+        btConfiguracoesAlterarNome = findViewById(R.id.btConfiguracoesAlterarNome);
+        btConfiguracoesAlterarSenha = findViewById(R.id.btConfiguracoesAlterarSenha);
+        dados = (Dados) getApplicationContext();
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        btConfiguracoesAlterarNome.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                if (!etConfiguracoesNome.getText().toString().equals("")) {
+                    final String nome = etConfiguracoesNome.getText().toString();
+
+                    new Thread(new Runnable() {
+                        @Override
+                        public void run() {
+                            try {
+                                dados.getOut().writeObject("MudarNome");
+                                dados.getIn().readObject();
+                                dados.getOut().writeObject(nome);
+
+                                runOnUiThread(new Runnable() {
+                                    @Override
+                                    public void run() {
+                                        Toast.makeText(dados, "Nome alterado com sucesso!", Toast.LENGTH_SHORT).show();
+                                    }
+                                });
+
+                            } catch (IOException | ClassNotFoundException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                    }).start();
+
+                } else {
+                    etConfiguracoesNome.setError("Informe o nome!");
+                    etConfiguracoesNome.requestFocus();
+                }
             }
         });
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        btConfiguracoesAlterarSenha.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(dados, "Show de bola", Toast.LENGTH_SHORT).show();
+                Toast.makeText(dados, ""+dados.getUser().getSenha(), Toast.LENGTH_SHORT).show();
+            }
+        });
     }
+
+
 
 }
